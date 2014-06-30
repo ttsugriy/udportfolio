116f6cedf273d095261210f400371f75f5de92e6
Taras Tsugrii <taras.tsugriy@gmail.com>
2014-06-30 02:49:11 +0000

    lint only perfmatters.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
   // Grunt configuration
   grunt.initConfig({
     jshint: {
-      all: ['Gruntfile.js']
+      all: ['js/perfmatters.js']
     },
 
     pagespeed: {
